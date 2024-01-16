58fc7ad661790a8147e6f2922a82eb9d2b214df0
victorMenaFlores <menaflores.victor@gmail.com>
2024-01-16 12:41:19 +0100

    +Creación de la clase TPV

---
+++ src/main/java/daw/TPV.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author victor
+ */
+public class TPV {
+    //Atributos
+    String identificador;
+    String ubicacion;
+    LocalTime fechaHora;
+    //ArrayList<ticket> regristroCompra;
+    String contraseña;
+    /*
+    Una minúscula (a - z)
+    Una mayúscula (A - Z)
+    Un número (0 - 9)
+    Uno de los siguientes caracteres especiales: # $ % &  ( ) * + , - .  : ; < = > @
+
+    */
+    //Constructores
+    
+    //SETTERS AND GETTERS
+    
+    //TO STRING
+    
+    //EQUALS Y HASHCODE
+}
