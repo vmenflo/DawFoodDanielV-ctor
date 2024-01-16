c00c3c1709bbd43b6d0b2b34e09197e8559e4043
Daniel <dnavbor167@g.educaand.es>
2024-01-16 18:25:11 +0100

    Creación de enum de Iva

---
+++ src/main/java/daw/Iva.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author danielnavasborjas
+ */
+public enum Iva {
+    IVA_REFRESCOS(0.21), 
+    IVA_ALCOHOL(0.21),
+    IVA_COMIDA(0.1), 
+    IVA_POSTRES(0.1), 
+    IVA_RESTOBEBIDA0(0.1);
+    
+    //Creación de atributo encapsulado
+    private double cantidadIva;
+
+    //Creación de constructor
+    private Iva(double cantidadIva) {
+        this.cantidadIva = cantidadIva;
+    }
+}
