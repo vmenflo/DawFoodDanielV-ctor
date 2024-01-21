2923905e0de16101b0de71f50799f9ad2fc78b4c
Daniel <dnavbor167@g.educaand.es>
2024-01-21 17:20:46 +0100

    Creación de clase "ListasProductos"

---
+++ src/main/java/daw/productos/ListasProductos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw.productos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danielnavasborjas
+ */
+public class ListasProductos {
+    //Creamos los atributos encapsulados de tipo ArrayList
+    private ArrayList<Comida> listaComida;
+    private ArrayList<Bebida> listaBebida;
+    private ArrayList<Postres> listaPostres;
+    
+    //Creamos constructor por defecto
+
+    public ListasProductos() {
+        
+    }
+    
+}
