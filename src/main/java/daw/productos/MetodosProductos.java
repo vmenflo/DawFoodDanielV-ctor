514366f7adf7e9c1842e3f76e3f8a7d0f26c73ba
dnavbor167 <dnavbor167@g.educaand.es>
2024-01-26 10:11:51 +0100

    Testing de diálogo menú usuario

--- src/main/java/daw/productos/MetodosProductos.java
+++ src/main/java/daw/productos/MetodosProductos.java
@@ -21,7 +21,7 @@ public class MetodosProductos {
         //Creamos un array de String para hacer un desplegable y que elija qué
         //es lo que quiere cambiar
         String[] opciones = {"Descripción", "Subcategoría", "Precio", "Iva", "stock"};
-        
+
         String eleccion = (String) JOptionPane.showInputDialog(null,
                 "Elige el campo que quiere modificar:",
                 "Seleccione para modificar",
@@ -37,13 +37,13 @@ public class MetodosProductos {
 
         return eleccion;
     }
-    
+
     //Método para que elija entre las categorías que hay
-    public static String elegirCategorias(){
+    public static String elegirCategorias() {
         //Creamos un array de String para hacer un desplegable y que elija 
         //la categoría
         String[] opciones = {"Comida", "Bebida", "Postres"};
-        
+
         String eleccion = (String) JOptionPane.showInputDialog(null,
                 "Elige la categoría:",
                 "Seleccione Categoría",
@@ -68,7 +68,6 @@ public class MetodosProductos {
                 "Selección de subcategoría",
                 JOptionPane.QUESTION_MESSAGE, null,
                 opciones, opciones[0]);
-       
         return eleccion;
     }
 
@@ -115,41 +114,41 @@ public class MetodosProductos {
                 elegir[1]);
         return mayorOMenorA18;
     }
-     
+
     //Hacemos un método donde estén todos los datos cargados
-    public static ListasProductos datosProductos(){
+    public static ListasProductos datosProductos() {
         //Creamos los productos principales
         //Comidas:
-        Comida c1 = new Comida(1, "McPollo", 
-                SubCategoriaComida.POLLO, 2.5, 
+        Comida c1 = new Comida(1, "McPollo",
+                SubCategoriaComida.POLLO, 2.5,
                 Iva.IVA_COMIDA, 150);
-        Comida c2 = new Comida(2, "McTernera", 
-                SubCategoriaComida.TERNERA, 3.2, 
+        Comida c2 = new Comida(2, "McTernera",
+                SubCategoriaComida.TERNERA, 3.2,
                 Iva.IVA_COMIDA, 150);
-        Comida c3 = new Comida(3, "McPescado", 
-                SubCategoriaComida.PESCADO, 2.1, 
+        Comida c3 = new Comida(3, "McPescado",
+                SubCategoriaComida.PESCADO, 2.1,
                 Iva.IVA_COMIDA, 150);
         //Bebidas:
-        Bebida b1 = new Bebida(1, "Agua natural", 
-                SubCategoriaBebida.AGUA, 1, 
+        Bebida b1 = new Bebida(1, "Agua natural",
+                SubCategoriaBebida.AGUA, 1,
                 Iva.IVA_REFRESCOS, 200);
-        Bebida b2 = new Bebida(2, "Zumo Naranja", 
-                SubCategoriaBebida.ZUMONARANZA, 1.5, 
+        Bebida b2 = new Bebida(2, "Zumo Naranja",
+                SubCategoriaBebida.ZUMONARANZA, 1.5,
                 Iva.IVA_RESTOBEBIDAS, 50);
-        Bebida b3 = new Bebida(3, "Cerveza", 
-                SubCategoriaBebida.CERVEZA, 1.6, 
+        Bebida b3 = new Bebida(3, "Cerveza",
+                SubCategoriaBebida.CERVEZA, 1.6,
                 Iva.IVA_ALCOHOL, 200);
         //Postres:
-        Postres p1 = new Postres(1, "Milk Shake", 3.5, 
+        Postres p1 = new Postres(1, "Milk Shake", 3.5,
                 Iva.IVA_POSTRES, 100);
-        Postres p2 = new Postres(2, "Browni", 3, 
+        Postres p2 = new Postres(2, "Browni", 3,
                 Iva.IVA_POSTRES, 300);
-        Postres p3 = new Postres(3, "Gofres", 3.8, 
+        Postres p3 = new Postres(3, "Gofres", 3.8,
                 Iva.IVA_POSTRES, 178);
-        
+
         //Instanciamos el objeto
         ListasProductos listaDeProductos = new ListasProductos();
-        
+
         //Añadimos todos los objetos a sus listas:
         listaDeProductos.añadirUnElemento(c1);
         listaDeProductos.añadirUnElemento(c2);
@@ -160,7 +159,7 @@ public class MetodosProductos {
         listaDeProductos.añadirUnElemento(p1);
         listaDeProductos.añadirUnElemento(p2);
         listaDeProductos.añadirUnElemento(p3);
-        
+
         return listaDeProductos;
     }
 }
